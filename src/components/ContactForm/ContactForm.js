0e44ca7ba141775981fc34c3c5cf2e87eee0a336
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2022-05-23 14:55:09 +0100

    fixed contact field in coming soon page

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -6,24 +6,41 @@ function ContactForm() {
 		<div>
 			<form action="" className="form-body">
 				<div className="contact-form center">
-					<label htmlFor="first name" className="first-name">
+					<label
+						htmlFor="first name"
+						className="first-name mobile-width"
+					>
 						<input
 							type="text"
 							placeholder="First Name.."
 							className="input-field"
 						/>
 					</label>
-					<label htmlFor="Last name" className="last-name">
+					<label
+						htmlFor="Last name"
+						className="last-name mobile-width"
+					>
 						<input
 							type="text"
 							placeholder="Last Name.."
 							className="input-field"
 						/>
 					</label>
+					<label
+						htmlFor="email"
+						className="email center mobile-width"
+					>
+						<input
+							type="email"
+							name="email"
+							id=""
+							placeholder="Enter email to subscribe"
+						/>
+						<div className="subscribe">
+							<h3>Subscribe</h3>
+						</div>
+					</label>
 				</div>
-				<label htmlFor="email" className="email center">
-					<input type="email" name="email" id="" />
-				</label>
 			</form>
 		</div>
 	);
--- src/pages/comingsoon.js
+++ src/pages/comingsoon.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HeroSection from "../components/HeroSection/HeroSection";
-import Footer from "../components/Footer/Footer";
+
 function ComingSoon() {
 	return (
 		<div className="home">
